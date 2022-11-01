2e75817088e9dc6cf6691e987b6b2a7255c4051c
nayemalam110 <nayemalam110@gmail.com>
2022-11-01 07:49:55 +0600

    step-1.5.5.1

--- lib/api/api_key.dart
+++ lib/api/api_key.dart
@@ -1,1 +1,1 @@
-const ApiKey = "111e76f07e2c48a7d42b3fedbf8f9f4f";
+const apiKey = "111e76f07e2c48a7d42b3fedbf8f9f4f";
--- lib/model/weather_data.dart
+++ lib/model/weather_data.dart
@@ -2,5 +2,5 @@ import 'package:my_weather_app/model/weather_data_current.dart';
 
 class WeatherData {
   final WeatherDataCurrent current;
-  WeatherData({required this.current});
+  WeatherData(this.current);
 }
